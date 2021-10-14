e3927d725fc2e2b5287f291a5131948c5b6333b0
s0npaRi11 <paragmahale36@gmail.com>
2021-10-14 18:06:40 +0530

    sitemap added

---
+++ src/sitemap-routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+
+export default (
+    <Route>
+        <Route path="/" />
+        <Route path="/about" />
+        <Route path="/contact" />
+        <Route path="/products" />
+        <Route path="/terms" />
+        <Route path="/products/men" />
+        <Route path="/products/women" />
+        <Route path="/products/kids" />
+        <Route path="/products/cotton-t-shirts" />
+        <Route path="/products/synthetic-t-shirts" />
+    </Route>
+)
